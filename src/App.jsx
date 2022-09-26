dd7098e2a65bde0d2bb6cb66832a81f593ee0fca
nicoelias997 <nicoelias997@gmail.com>
2022-09-26 15:47:58 +0200

    Agrego las funciones agregarTarea, eliminarTarea y subo a localStorage, tengo que crear mas componentes y hacer responsive

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Tarea from "./components/Tarea";
+
+function App() {
+  return (
+    <div>
+      <h1 className="m-2 mb-5">
+        TP8 - Ejercicio 4
+      </h1>
+        <Tarea></Tarea>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tarea.jsx
@@ -0,0 +1,82 @@
+import React  from "react";
+import { useEffect, useState } from "react";
+
+const Tarea = () => {
+
+    const tareasLocalStorage = JSON.parse(localStorage.getItem("listaTareas")) || [];
+    const [tarea, setTarea] = useState("")
+    const [tareas, setTareas] = useState(tareasLocalStorage)
+
+    
+    //ciclo de vida componente
+    useEffect(() => {
+      localStorage.setItem("listaTareas", JSON.stringify(tareas))
+    },[tareas])
+
+  const agregarTarea = () => {
+    if(!tarea.trim()){
+        console.log("Error")
+        return
+    } else {
+        setTareas([
+            ...tareas, 
+        {nombreTarea: tarea}
+        ])
+    setTarea("")
+    }
+  };
+
+  const eliminarTarea = (tarea) => {
+    const arrayFiltrado = tareas.filter(item => item.nombreTarea !== tarea)
+    setTareas(arrayFiltrado)
+}
+
+  return (
+    <div className="row d-flex m-2">
+      <div className="col-12 text-center">
+        <h3>Ingrese su tarea</h3>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Ingrese una tarea"
+            className="form-control"
+            aria-label="Recipient"
+            aria-describedby="boton"
+            onChange={e => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <button
+            type="button"
+            id="boton"
+            onClick={() => agregarTarea()}
+            className="btn btn-danger btn-sm"
+          >
+            Enviar tarea
+          </button>
+        </div>
+      </div>
+      <div className="row col-6 mt-4">
+        <h3>Lista de tareas</h3>
+        <div >
+          <ul className="list-group">
+          {
+            tareas.map(item => (
+                <li className="list-group-item" key={item.nombreTarea}>
+                    <span className="lead">{item.nombreTarea}</span>
+                    <button 
+                    className="btn btn-danger btn-sm float-end mx-2"
+                    onClick={()=> eliminarTarea(item.nombreTarea)}>
+                    Eliminar
+                    </button>
+                </li>
+                ))
+            }
+           
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tarea;
