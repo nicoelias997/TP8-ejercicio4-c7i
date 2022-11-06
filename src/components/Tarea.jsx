220e818dd3d976232c026b2f385fc754a8928bb5
nicoelias997 <nicoelias997@gmail.com>
2022-11-06 22:46:52 +0100

    obtengo la tarea por id pero me falta poder editarla cuando escribo algo diferente

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -1,80 +1,206 @@
 import React  from "react";
 import { useEffect, useState } from "react";
-import {consultarAPI, crearTareaApi} from "./queries"
+import { Form, Button } from "react-bootstrap";
+import {  useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import {borrarTareaApi, consultarAPI, crearTareaApi, editarTareaAPI, obtenerTareaApi} from "./queries"
 
 const Tarea = () => {
 
-    const tareasLocalStorage = JSON.parse(localStorage.getItem("listaTareas")) || [];
-    const [tarea, setTarea] = useState("")
-    const [tareas, setTareas] = useState(tareasLocalStorage)
+    // const tareasLocalStorage = JSON.parse(localStorage.getItem("listaTareas")) || [];
+    const [tareas, setTareas] = useState([])
+    const [id, setId] = useState("")
 
+    const [modoEdicion, setModoEdicion] = useState(false)
+
+
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      setValue,
+      reset, 
+      setFocus
+    } = useForm();
+
+    setFocus("nombreTarea");
     
     //ciclo de vida componente
     useEffect(() => {
       consultarAPI().then(respuesta => {
         setTareas(respuesta)
       })
-    },[tareas])
+    },[])
 
-  const agregarTarea = () => {
-    if(!tarea.trim()){
-        console.log("Error")
-        return
-    } else {
-        crearTareaApi(tarea).then(respuesta => {
+  const agregarTarea = (tarea) => {
+      crearTareaApi(tarea).then(respuesta => {
           if(respuesta.status === 201){
-            setTareas(tarea)
-          } else{
-            console.log("Ocurrio un error")
-          }
+            Swal.fire(
+              "Producto creado",
+              "El producto fue creado exitosamente",
+              "success"
+            )
+          reset()
+          consultarAPI().then((respuesta) => {
+            setTareas(respuesta);
+          });
+        } 
+        if(respuesta.status === 400) {
+          Swal.fire("Hubo un error", "No pudimos agregar la tarea", "error")
+          return
         }
-        )
-        setTarea("")
+      })
     }
-  };
+  
 
-  const eliminarTarea = (tarea) => {
-    const arrayFiltrado = tareas.filter(item => item.nombreTarea !== tarea)
-    setTareas(arrayFiltrado)
+  const eliminarTarea = (id) => {
+    borrarTareaApi(id).then(respuesta => {
+      if(respuesta.status === 200){
+          Swal.fire(
+            "Tarea eliminada",
+            "La tarea fue eliminada exitosamente",
+            "success"
+          )
+          reset()
+          consultarAPI().then(respuesta => {
+            setTareas(respuesta)
+          })
+      } else{
+        console.log("no se pudo eliminar")
+      }
+    })
 }
+   
+const obtenerTarea = _id => {
+
+  obtenerTareaApi(_id).then((respuesta) => {
+    if(respuesta.status === 200){
+      setValue("nombreTarea", respuesta.dato.nombreTarea)
+      setModoEdicion(true)
+    }})
+  }
+
+  const editarTarea = (datos) => {
+    editarTareaAPI(id, datos.nombreTarea).then(respuesta => {
+      if(respuesta.status === 200){
+          Swal.fire(
+            "Tarea editada",
+            "La tarea fue editado exitosamente",
+            "success"
+          )
+        consultarAPI().then(respuesta => {
+          setTareas(respuesta)
+        })
+        setModoEdicion(false)
+        reset()
+      } 
+      if(respuesta.status === 400) {
+        Swal.fire("Hubo un error", "No pudimos agregar la tarea", "error")
+        return
+      }
+    })
+  }
+   
 
   return (
     <div className="row d-flex m-2">
       <div className="col-12 text-center">
         <h3>Ingrese su tarea</h3>
-        <div className="input-group">
-          <input
+      {
+        modoEdicion ? 
+        <>
+        <Form xs={12} onSubmit={handleSubmit(editarTarea)}>
+          <Form.Group>
+          <Form.Text className="text-danger float-start mb-2">
+                {errors.nombreTarea?.message}
+              </Form.Text>
+            <Form.Control
             type="text"
             placeholder="Ingrese una tarea"
-            className="form-control"
             aria-label="Recipient"
             aria-describedby="boton"
-            onChange={e => setTarea(e.target.value)}
-            value={tarea}
-          />
-          <button
-            type="button"
+            {
+              ...register("nombreTarea", {
+                required: "Debe ingresar una tarea",
+                minLength: {
+                  value: 1,
+                  message: "El nombre debe tener al menos 1 caracter",
+                }
+              })
+            }
+            
+            >
+            </Form.Control>
+         
+            <Button
+            type="submit"
             id="boton"
-            onClick={() => agregarTarea()}
-            className="btn btn-danger btn-sm"
+            className="btn btn-warning mt-2"
+          >
+            Editar tarea
+         </Button>
+          </Form.Group>
+          </Form>
+        </>
+        : 
+        <>
+        <Form xs={12} onSubmit={handleSubmit(agregarTarea)}>
+          <Form.Group>
+          <Form.Text className="text-danger float-start mb-2">
+                {errors.nombreTarea?.message}
+              </Form.Text>
+            <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            aria-label="Recipient"
+            aria-describedby="boton"
+            {
+              ...register("nombreTarea", {
+                required: "Debe ingresar una tarea",
+                minLength: {
+                  value: 1,
+                  message: "El nombre debe tener al menos 1 caracter",
+                }
+              })
+            }
+            
+            >
+            </Form.Control>
+         
+           
+              <Button
+            type="submit"
+            id="boton"
+            className="btn btn-danger mt-2"
           >
             Enviar tarea
-          </button>
-        </div>
+          </Button>
+          </Form.Group>
+          </Form>
+        </>
+      }
+          
       </div>
-      <div className="row col-12 col-sm-6 mt-4">
-        <h3>Lista de tareas</h3>
+      <div className="row col-12">
+        <h3 className="float-start">Lista de tareas</h3>
         <div >
           <ul className="list-group">
           {
             tareas.map(item => (
-                <li className="list-group-item" key={item.nombreTarea}>
+                <li className="list-group-item" key={item._id}>
                     <span className="lead">{item.nombreTarea}</span>
+                    <div className="float-end">
                     <button 
                     className="btn btn-danger btn-sm float-end mx-2"
-                    onClick={()=> eliminarTarea(item.nombreTarea)}>
+                    onClick={()=> eliminarTarea(item._id)}>
                     Eliminar
                     </button>
+                    <button 
+                    className="btn btn-warning btn-sm float-end mx-2"
+                    onClick={()=> obtenerTarea(item._id)}>
+                    Edit
+                    </button>
+                    </div>
                 </li>
                 ))
             }
